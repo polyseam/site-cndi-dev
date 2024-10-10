d4a139b93771578f359f6716706fc75b165e7940
Matt Johnston <m@ttjohnston.com>
2024-10-10 00:48:48 -0300

    rm extra utils

--- utils.ts
+++ utils.ts
@@ -1,9 +1,1 @@
-import { data } from "template-details";
-
 export const capitalize = (s: string) => s.charAt(0).toUpperCase() + s.slice(1);
-
-export const getTemplateIdForName = (templateName: string) => {
-  const t = data.find((template) => template.name === templateName);
-  const templateId = t?.templateId;
-  return templateId ?? "";
-};
