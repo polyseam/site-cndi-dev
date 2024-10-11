8280d0e1c5e77e769ec6c247c2e1d6fd19f94a59
Matt Johnston <m@ttjohnston.com>
2024-10-11 20:22:56 -0300

    add privacy policy

---
+++ routes/privacy-policy.tsx
@@ -0,0 +1,420 @@
+export default function PrivacyPolicy() {
+  return (
+    <div>
+      <div class="container-1264 w-container">
+        <p class="paragraph-27">
+          <strong class="bold-text-4">
+            POLYSEAM DATA CORP. PRIVACY POLICY
+            <br />
+          </strong>
+          <br />
+          LAST UPDATED: September 19th, 2022
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            1. OVERVIEW
+            <br />
+          </strong>
+          <br />
+          At POLYSEAM DATA CORP. (the “Company”, “we” or “us”), we use our
+          website and other
+          <br />
+          internet-based tools for the following purpose:
+          <br />
+          <br />
+          1. To provide information on our products
+          <br />
+          2. To connect with users of the products
+          <br />
+          3. To evaluate the performance of our products
+          <br />
+          <br />
+          We are required to collect and retain information about the people and
+          companies that access and use the website (“Users” or “you”). This
+          Policy applies to our privacy practices on all communications between
+          the Company and Users, including but not limited to communication via:
+          <br />
+          <br />
+          1. The Company website (including the website hosted at cndi.run and
+          any other web domain used by the Company to communicate with users now
+          or in the future);
+          <br />
+          2. Any mobile, computer, or tablet-based application currently offered
+          by Company or developed in the future by the Company; and
+          <br />
+          3. All other services provided by the Company, as described on the
+          Website (Items 1 to 3 are collectively referred to as the
+          &quot;Website&quot;).
+          <br />
+          ‍<br />
+          This Privacy Policy (“Policy”) sets out how we use, protect, collect
+          and preserve
+          <br />
+          information submitted by Users, including User information gathered by
+          our technology. This Policy should be read in conjunction with any
+          Terms of Use or policies available on the Website.
+          <br />
+          ‍<br />
+          By accessing or submitting any information through the Website, you
+          are agreeing to the terms of this Policy. You are prohibited from
+          accessing or using the Website unless you fully understand and agree
+          to this Policy and all Terms of Use available for review on the
+          Website.
+          <br />
+          ‍<br />
+          If you have any questions about this Policy, please contact us by
+          sending an email with your questions to info@polyseam.io before
+          accessing or entering any information on the Website.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            2. INFORMATION ABOUT USERS WE COLLECT AND STORE
+            <br />
+          </strong>
+          ‍<br />
+          When you access and use the Website, we will collect and retain all
+          personal identifying information submitted to us.
+          <br />
+          ‍<br />
+          We will take steps to protect the information submitted to us by Users
+          and we will not disclose any personal identifying information without
+          the consent of Users, unless otherwise set out in this Policy.
+          <br />
+          ‍<br />
+          Personal identifying information may include but is not necessarily
+          limited to:
+          <br />
+          <br />
+          1. Name
+          <br />
+          2. Email
+          <br />
+          3. IP Address
+          <br />
+          4. Error information from the user&#x27;s experience <br />
+          5. Anonymized identifier for the user&#x27;s device
+          <br />
+          ‍<br />
+          We will not use personal identifying information to contact you for
+          any reason unrelated to your use of the Website or for commercial
+          reasons unrelated to the business of the Company. Except with your
+          consent or where required by law, we will not provide personal
+          identifying information to any third party.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            3. UPLOADING PERSONAL INFORMATION
+            <br />
+          </strong>
+          <br />
+          In order to facilitate their interactions on the Website, Users are
+          able, where they deem appropriate, to upload information including
+          personal identifying information. Users should only upload information
+          to the extent that such information enhances their experience of using
+          the Website.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            4. ADDITIONAL DATA COLLECTED
+            <br />
+          </strong>
+          ‍<br />
+          To learn about your experience as a User and to make periodic
+          improvements to the Website and our business, we may collect and
+          retain certain information. We will automatically record information
+          about your activity on the Website. This may include your Internet
+          Protocol (IP) address, operating system, geolocation information,
+          locale preferences, identification numbers associated with your
+          computers and mobile devices, your mobile carrier, date and time
+          stamps associated with transactions, system configuration information,
+          metadata, and other electronic interactions with the Company and the
+          Website.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            5. COOKIES
+            <br />
+          </strong>
+          ‍<br />
+          To facilitate the collection of information and to enhance the
+          experience of Users of the Website, we use cookies which may be stored
+          on Users’ computers and/or devices so that we can recognize Users when
+          they return. You may set your browser to notify you when you receive a
+          cookie or to not accept certain cookies. However, changing such
+          settings may result in you not being able to use the Website, either
+          in its entirety or certain features.
+          <br />
+          ‍<br />
+          Using cookies, you may have the option to allow us to save your user
+          ID and login
+          <br />
+          password for future logins to the Website.
+          <br />
+          ‍<br />
+          Cookies that are stored on your computer and/or device after visiting
+          the Website may be recognized for advertising purposes and to show you
+          targeted advertisements after you visit the Website.
+          <br />
+          ‍<br />
+          For more information on the cookies we use or to opt-out, please
+          contact us at
+          <br />
+          info@polyseam.io.
+          <br />
+          <br />
+          <strong class="privacy-heading">6. REMARKETING</strong>
+          <br />
+          ‍<br />
+          We may engage in &quot;remarketing&quot; activities using third-party
+          services, which may include but are not limited to Google Ads, Google
+          Analytics, and Facebook. By using the Website, you consent to allow us
+          to provide information to third-party service parties who may present
+          you with advertisements about the Company after you visit the Website.
+          We will provide third-party providers of remarketing services with the
+          minimum amount of information required to facilitate remarketing, and
+          in no circumstances will Company provide any personal identifying
+          information to third-parties for the purpose of remarketing.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            7. PERMITTED USES OF DATA
+            <br />
+          </strong>
+          ‍<br />
+          We use data collected from the Website for various purposes, including
+          to: provide the Website to you and accompanying customer support;
+          notify you about updates and changes to the Website; prevent and
+          address technical issues with the Website; monitor usage of the
+          Website.
+          <br />
+          ‍<br />
+          Without restriction, we may use any information you submit through the
+          Website for any purpose we deem appropriate so long as it is anonymous
+          and stripped of any and all personal identifying information.
+          <br />
+          ‍<br />
+          We may use the data collected from the Website, devices, browsers, and
+          any other sources, for its own purposes, both related to and unrelated
+          to improving the Website.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            8. LEGAL BASIS FOR PROCESSING PERSONAL IDENTIFYING INFORMATION
+            <br />
+          </strong>
+          <br />
+          We may process your personal identifying information for a number of
+          reasons, including:
+          <br />
+          <br />
+          performing a contract with you relating to the Website;
+          <br />
+          ‍<br />
+          you have provided us consent to do so;
+          <br />
+          ‍<br />
+          - fulfilling a legal obligation;
+          <br />
+          - fulfilling our legitimate interests, which are not overridden by
+          your rights;
+          <br />
+          - for payment processing purposes.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            9. ACCESSING, CORRECTING &amp; DELETING YOUR PERSONAL INFORMATION
+            <br />
+          </strong>
+          <br />
+          If you wish to access or amend your personal information or to request
+          that we
+          <br />
+          permanently delete your personal information from our servers, please
+          send an email to info@polyseam.io. We will ensure your personal
+          information is accessible, amended and/or deleted in a reasonable time
+          after receiving that request. Deleting such information may require us
+          to terminate any account you may have created in its entirety and
+          prevent you from using the Website.
+          <br />
+          <br />
+          So long as your account remains active, we will preserve your personal
+          information in accordance with the Policy.
+          <br />
+          <br />
+          You may decline to share certain personal information with us, in
+          which case you may not be able to use the Website, either in its
+          entirety or certain features.
+          <br />
+          <br />
+          When deleting your information, whether by request or due to
+          inactivity, we will use standard electronic means to remove your
+          personal information from our files.
+          <br />
+          <br />
+          We will also retain any and all information that we are required to
+          retain under any
+          <br />
+          applicable laws for the full duration of time required under those
+          laws.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            10. KEEPING YOUR DATA SECURE
+            <br />
+          </strong>
+          <br />
+          We will store your information in electronic format on
+          industry-standard servers that we have selected. Except where required
+          to do so by law, we will not store your personal information in any
+          physical or paper-based format.
+          <br />
+          ‍<br />
+          The servers we select to store information engage security software
+          and generally
+          <br />
+          accepted standards to protect personal information. The software used
+          by these servers is intended to prevent unauthorized access or
+          improper use.
+          <br />
+          <br />
+          However, no security system is perfect and there is always a risk of
+          unauthorized parties accessing your personal information. Therefore,
+          we make no guarantees or representations as to the technical or legal
+          compliance of the servers we use. We cannot guarantee the complete
+          protection and security of your personal information.
+          <br />
+          <br />
+          Our servers may be located outside of Canada, and thus are potentially
+          subject to the privacy and security laws in force in other countries.
+          If you have concerns about your personal information being stored
+          electronically in foreign jurisdictions, please contact us before
+          proceeding to use the Website.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            11. SECURITY BREACHES
+            <br />
+          </strong>
+          <br />
+          In the event the personal information of a User is disclosed as the
+          result of any breach of security of the Website or its servers,
+          regardless of who is at fault, we will take steps to advise all known
+          affected Users within a reasonable timeframe of learning of the
+          breach.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            12. THIRD-PARTY APPLICATIONS
+            <br />
+          </strong>
+          <br />
+          To provide an enhanced experience to Users, including to track
+          conversions and analytics, we may engage services and features
+          controlled by third-parties. We cannot control or dictate how any
+          third-party application you use in conjunction with the Website, now
+          or at any point in the future, will treat any personal information you
+          transmit through their servers when using the Website. We advise you
+          to contact these parties directly to review their privacy policies.
+          <br />
+          ‍<br />
+          Without limitation, the Company uses or may use the following
+          third-party applications and services in conjunction with the Website:
+          <br />
+          <br />
+          1. Google Analytics
+          <br />
+          2. Hubspot
+          <br />
+          3. Webflow Forms
+          <br />
+          <br />
+          By using any of these third-party application to interact with the
+          Website, we will not be given access to your login credentials, user
+          name, password, payment information, or any information you may
+          provide directly to these applications.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            13. USE OF WEBSITE BY MINORS
+            <br />
+          </strong>
+          <br />
+          The Website is not intended to be accessed by persons under the age of
+          18. The Company does not engage in any marketing efforts aimed at
+          children or minors.
+          <br />
+          <br />
+          Persons under the age of 18 are prohibited from using the Website in
+          any manner or providing any information to Company. In the event
+          Company is provided with any information, including personal
+          identifying information, relating to a minor, that
+          <br />
+          information will be immediately deleted without notice.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            14. DISCLOSURE OF PERSONAL INFORMATION
+            <br />
+          </strong>
+          <br />
+          In certain situations, we may be required to disclose personal data in
+          response to lawful requests by public authorities, including to meet
+          national security or law enforcement requirements.
+          <br />
+          <br />
+          We may disclose your personal information to third parties:
+          <br />
+          <br />
+          1. In the event that we sell or buy any business or assets, in which
+          case we may disclose
+          <br />
+          your personal data to the prospective seller or buyer of such business
+          or assets.
+          <br />
+          <br />
+          2. If the Company or its assets are acquired by a third party, in
+          which case personal data
+          <br />
+          about Users may be one of the transferred assets.
+          <br />
+          <br />
+          3. If we are under a duty to disclose or share your personal data in
+          order to comply with
+          <br />
+          any legal obligation, or in order to enforce or apply our Terms of
+          Use; or to protect
+          <br />
+          the rights, property, or safety of the Company, our Users, or others.
+          This includes
+          <br />
+          exchanging information with other companies and organizations for the
+          purposes of fraud protection and credit risk reduction.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            15. AMENDING THIS POLICY
+            <br />
+          </strong>
+          <br />
+          This Policy may be updated and amended from time to time. We reserve
+          the right to change this Policy at any time, and any amended Policy is
+          effective upon posting to the Website. We will make efforts to
+          communicate any changes to this Policy we deem material, in our sole
+          discretion, via email or notifications on the Website. Your continued
+          use of the Website will be deemed acceptance of any amended Policy.
+          <br />
+          <br />
+          <strong class="privacy-heading">
+            16. CONTACT US
+            <br />
+          </strong>
+          <br />
+          We encourage you to send us questions and inquiries on this Policy and
+          the steps we take to keep your personal information secure. Please
+          send us an email: info@polyseam.io We will respond to all inquiries on
+          the Policy within 7 days.
+        </p>
+      </div>
+    </div>
+  );
+}
